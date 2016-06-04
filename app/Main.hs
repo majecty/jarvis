f9010acc2828db6e9dcbf70170175456302b86f2
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-06-04 19:51:03 +0900

    Setup optparse-generic

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Monad
+import Options.Generic
 import System.Environment
 import System.Exit
 
@@ -8,7 +12,7 @@ import Jarvis
 
 main :: IO ()
 main = do
-  args <- getArgs
-  errs <- jarvis args
+  args <- getRecord "jarvis"
+  errs <- jarvis (args :: [String])
   unless (null errs) $
       exitWith $ ExitFailure 1
