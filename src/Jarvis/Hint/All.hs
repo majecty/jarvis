30bff4ebefbe647788afc6a7320911cf539ab683
Jooyung Han <jooyung.han@gmail.com>
2016-06-09 00:58:08 +0900

    Add BadComparisonWithBool to AllHints

--- src/Jarvis/Hint/All.hs
+++ src/Jarvis/Hint/All.hs
@@ -6,14 +6,17 @@ module Jarvis.Hint.All
 
 import Jarvis.Hint.Type
 import Jarvis.Hint.BadCovariantDefinitionOfEquals
+import Jarvis.Hint.BadComparisonWithBoolean
 
 data HintBuiltin =
     HintBadCovariantDefinitionOfEquals
+    | HintBadComparisonWithBoolean
   deriving (Show,Eq,Ord,Bounded,Enum)
 
 builtin :: HintBuiltin -> Hint
 builtin x = case x of
   HintBadCovariantDefinitionOfEquals -> typeDecl badCovariantDefinitionOfEqualsHint
+  HintBadComparisonWithBoolean -> typeDecl badComparisonWithBooleanHint
   where
     typeDecl x = mempty{hintTypeDecl=x}
 
